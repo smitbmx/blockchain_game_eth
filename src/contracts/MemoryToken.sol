3e239bdfde04e9579cd8d7986703fd65bf6c0e03
Ivan Brygar <smitbmx@gmail.com>
2020-11-15 13:25:48 +0800

    Extended MemoryToken with ERC721 and written tests.

--- src/contracts/MemoryToken.sol
+++ src/contracts/MemoryToken.sol
@@ -2,6 +2,6 @@ pragma solidity ^0.5.0;
 
 import "./ERC721Full.sol";
 
-contract MemoryToken {
-    string public name = "Memory token";
+contract MemoryToken is ERC721Full {
+    constructor() public ERC721Full("Memory Token", "MEMORY") {}
 }
--- test/MemoryToken.test.js
+++ test/MemoryToken.test.js
@@ -1,9 +1,31 @@
-const MemoryToken = artifacts.require('./MemoryToken.sol')
+const MemoryToken = artifacts.require("./MemoryToken.sol");
 
-require('chai')
-  .use(require('chai-as-promised'))
-  .should()
+require("chai").use(require("chai-as-promised")).should();
 
-contract('Memory Token', (accounts) => {
-  // code goes here...
-})
+contract("Memory Token", (accounts) => {
+  let token;
+
+  before(async () => {
+    token = await MemoryToken.deployed();
+  });
+
+  describe("deployment", async () => {
+    it("deploys successfully", async () => {
+      const address = token.address;
+      assert.notEqual(address, 0x0);
+      assert.notEqual(address, "");
+      assert.notEqual(address, null);
+      assert.notEqual(address, undefined);
+    });
+
+    it("has a name", async () => {
+      const name = await token.name();
+      assert.equal(name, "Memory Token");
+    });
+
+    it("has a symbols", async () => {
+      const name = await token.symbol();
+      assert.equal(name, "MEMORY");
+    });
+  });
+});
