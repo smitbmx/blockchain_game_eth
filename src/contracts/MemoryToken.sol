e1ebcde05e4757c6317227636074445d7fc3f140
Ivan Brygar <smitbmx@gmail.com>
2020-11-16 18:12:16 +0800

    Added a mint function. Added tests for token distribution.

--- src/contracts/MemoryToken.sol
+++ src/contracts/MemoryToken.sol
@@ -4,4 +4,12 @@ import "./ERC721Full.sol";
 
 contract MemoryToken is ERC721Full {
     constructor() public ERC721Full("Memory Token", "MEMORY") {}
+
+    function mint(address _to, string memory _tokenURI) public returns (bool) {
+        uint256 _tokenId = totalSupply().add(1);
+        _mint(_to, _tokenId);
+        _setTokenURI(_tokenId, _tokenURI);
+
+        return true;
+    }
 }
