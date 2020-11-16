16ba31ef3f50c6a33644d7ba4f6ba2277f0e903b
Ivan Brygar <smitbmx@gmail.com>
2020-11-16 21:44:49 +0800

    Defined the loadWeb3 function

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,19 @@ import MemoryToken from "../abis/MemoryToken.json";
 import brain from "../brain.png";
 
 class App extends Component {
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
+    }
+  }
+
   constructor(props) {
     super(props);
     this.state = {
