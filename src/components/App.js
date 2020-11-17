d5db0ecf61974401023d36b21145a5abe1961b30
Ivan Brygar <smitbmx@gmail.com>
2020-11-17 21:40:33 +0800

    Retrieve token info from blockchain.

--- src/components/App.js
+++ src/components/App.js
@@ -27,12 +27,38 @@ class App extends Component {
     const web3 = window.web3;
     const accounts = await web3.eth.getAccounts();
     this.setState({ account: accounts[0] });
+
+    //Load smart contract
+    const networkId = await web3.eth.net.getId();
+    const networkData = MemoryToken.networks[networkId];
+
+    if (networkData) {
+      const abi = MemoryToken.abi;
+      const address = networkData.address;
+      const token = new web3.eth.Contract(abi, address);
+      this.setState({ token });
+      const totalSupply = await token.methods.totalSupply().call();
+      this.setState({ totalSupply });
+
+      //Load Tokens
+      let balanceOf = await token.methods.balanceOf(accounts[0]).call();
+      for (let i = 0; i < balanceOf; i++) {
+        let id = await token.methods.tokenOfOwnerByIndex(accounts[0], i).call();
+        let tokenURI = await token.methods.tokenURI(id).call();
+        this.setState({ tokenURIs: [...this.state.tokenURIs, tokenURI] });
+      }
+    } else {
+      window.alert("Smart contract not deployed to detected network.");
+    }
   }
 
   constructor(props) {
     super(props);
     this.state = {
       account: "0x0",
+      token: null,
+      totalSupply: 0,
+      tokenURIs: [],
     };
   }
 
