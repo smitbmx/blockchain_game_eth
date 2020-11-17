d0bc145dc32b604ba198b7fdf70cd7fe103bdf61
Ivan Brygar <smitbmx@gmail.com>
2020-11-17 22:37:17 +0800

    Updated title

--- src/components/App.js
+++ src/components/App.js
@@ -208,7 +208,7 @@ class App extends Component {
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <h1 className="d-4">Edit this file in App.js!</h1>
+                <h1 className="d-4">Start matching now!</h1>
 
                 <div className="grid mb-4">
                   {this.state.cardArray.map((card, key) => {
